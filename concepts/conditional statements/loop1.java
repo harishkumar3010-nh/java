59e5e9a16bbfebe1737e66241d2e92cc558a8661
harishkumar3010-nh <59726078+harishkumar3010-nh@users.noreply.github.com>
2021-03-05 19:48:24 +0530

    Add files via upload

---
+++ concepts/conditional statements/loop1.java
@@ -0,0 +1,13 @@
+public class loop1 {
+    public static void main (String args[]){
+        int num=123;
+        int sum=0;
+        for(int i=0;i<=num;i++){
+            sum= sum+i;
+
+        }
+        System.out.println("sum of numbers=" + sum);
+
+    }
+    
+}
